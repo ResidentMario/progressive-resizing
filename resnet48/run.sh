6fd14113766cef9151cdfb4de2ecd2113506a951
ResidentMario <aleksey.bilogur@gmail.com>
2019-03-22 17:22:50 -0700

    Update to drop certain rare classes.

--- resnet48/run.sh
+++ resnet48/run.sh
@@ -5,5 +5,9 @@ then
         X_meta.csv --quiet
     aws s3 sync s3://quilt-example/quilt/open_fruit/images_cropped/ \
         images_cropped/ --quiet
+    rm -rf 'images_cropped/Mango'
+    rm -rf 'images_cropped/Common_fig'
+    rm -rf 'images_cropped/Cantaloupe'
+    rm -rf 'images_cropped/Pomegranate'
     python train.py
 fi
