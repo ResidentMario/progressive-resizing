59130ec59ef032a3f27765cd1a24926e73bc62ec
ResidentMario <aleksey.bilogur@gmail.com>
2019-03-25 20:55:59 -0700

    Add keras_lr_finder to dependencies.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-pip install keras pillow imbalanced-learn comet_ml t4
+pip install keras pillow imbalanced-learn comet_ml t4 keras_lr_finder
 pip install -U keras-preprocessing
 pip install git+https://github.com/ResidentMario/fahr
